a6d529575df09869ed5dc65da1d6801f11e19ac2
SciDev5 <67175934+SciDev5@users.noreply.github.com>
2020-09-05 17:10:08 -0400

    Add copyright statement to files.

--- src/me/scidev5/xmodtools/data/file/DataUtils.java
+++ src/me/scidev5/xmodtools/data/file/DataUtils.java
@@ -1,3 +1,24 @@
+/*
+ * XModTools  (A set of Java tools for using extended module song files.)
+ * Copyright (C) 2020  SciDev5 (https://github.com/SciDev5)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * License file should be called "COPYING" which is in the root of the
+ * master branch of theGitHub repositiory (https://github.com/SciDev5/Java-XModTools).
+ */
+
 package me.scidev5.xmodtools.data.file;
 
 import java.nio.ByteBuffer;
